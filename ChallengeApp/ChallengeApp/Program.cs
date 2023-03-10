5e4cc518a54db1607bc14ad1fd8df419122c6b82
Magdalena W <madzia21zdw@gmail.com>
2023-03-10 09:20:20 +0100

    Zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zmienna z imieniem
+string Name = "Ewa";
+//zmienna z plcia
+string sex = "Kobieta";
+//zmienna z wiekiem
+int myAge = 33;
+Console.WriteLine("Imie :"+ Name);
+Console.WriteLine("Plec :"+ sex);
+Console.WriteLine("Wiek :"+ myAge);
+
+if (Name == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "Kobieta" && myAge > 30)
+{
+    Console.WriteLine("KObieta, poniezej 30 lat");
+}
+else if (sex == "Mezczyzna" && myAge < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("warunek nie zostal spelniony");
+}
+
